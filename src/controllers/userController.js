3647a3ba6e34dca50c4fedb24cc89c7e7e6f91ab
Fakorede Oluwatobiloba <fakoredetobiloba.ft@gmail.com>
2021-12-18 00:44:33 +0100

    Fixed Error response in UserController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,12 @@
+const errorResponse = require("../response/error");
+
 const userController = (serviceContainer) => {
   const registerUser = async (req, res) => {
     try {
       const result = await serviceContainer.userService.addUser(req, res);
       return result;
     } catch (error) {
-      return res.code(500).send({
-        success: false,
-        message:
-          error instanceof Error ? error.message : "Something Went Wrong",
-      });
+      return errorResponse(res, error.message, 500, error);
     }
   };
 
